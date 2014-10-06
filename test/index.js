d13af3cc9c185a974d8cdf33d1f0226aa271de31
Adam Bretz <arbretz@gmail.com>
2014-10-06 11:34:08 -0400

    Updated code to limit backpressure.

--- test/index.js
+++ test/index.js
@@ -3,8 +3,8 @@
 var EventEmitter = require('events').EventEmitter;
 var Fs = require('fs');
 var Crypto = require('crypto');
-var Writable = require('stream').Writable;
 var Hoek = require('hoek');
+var Writable = require('stream').Writable;
 var Lab = require('lab');
 var lab = exports.lab = Lab.script();
 var GoodFile = require('..');
@@ -67,7 +67,7 @@ describe('good-file', function () {
         done();
     });
 
-    it('stop() ends the stream', function (done) {
+    it('stop() ends the stream and kills the queue', function (done) {
 
         var file = Hoek.uniqueFilename('./test/fixtures');
         var reporter = new GoodFile(file, {
@@ -80,12 +80,14 @@ describe('good-file', function () {
         reporter.start(ee, function (error) {
 
             expect(error).to.not.exist;
-            expect(reporter._currentStream.path).to.contain(file +  '.001');
+
+            ee.emit('report', 'request', { id: 1, timestamp: Date.now() });
 
             reporter.stop();
             expect(reporter._currentStream.bytesWritten).to.equal(0);
             expect(reporter._currentStream.path).to.contain(file +  '.001');
             expect(reporter._currentStream._writableState.ended).to.equal(true);
+            expect(reporter._queue.running()).to.equal(0);
 
             internals.removeLog(reporter._currentStream.path);
 
@@ -170,11 +172,12 @@ describe('good-file', function () {
                     expect(error).to.not.exist;
 
                     expect(reporter._currentStream.bytesWritten).to.equal(900);
+                    expect(reporter._currentStream._good.length).to.equal(900);
                     expect(reporter._currentStream.path).to.contain('/test/fixtures/' + file + '.001');
                     internals.removeLog(reporter._currentStream.path);
 
                     done();
-                }, 1000);
+                }, 2000);
             });
         });
 
@@ -201,9 +204,8 @@ describe('good-file', function () {
                 setTimeout(function () {
 
                     expect(reporter._currentStream.bytesWritten).to.equal(92);
+                    expect(reporter._currentStream._good.length).to.equal(92);
                     expect(reporter._currentStream.path).to.contain('/test/fixtures/' + file + '.004');
-
-                    expect(reporter._eventQueue).to.be.empty;
 
                     internals.removeLog('./test/fixtures/' + file + '.001');
                     internals.removeLog('./test/fixtures/' + file + '.002');
@@ -212,7 +214,7 @@ describe('good-file', function () {
 
                     done();
 
-                }, 1000);
+                }, 2000);
             });
         });
 
@@ -239,6 +241,7 @@ describe('good-file', function () {
                 setTimeout(function() {
 
                     expect(reporter._currentStream.bytesWritten).to.equal(530);
+                    expect(reporter._currentStream._good.length).to.equal(530);
                     expect(reporter._currentStream.path).to.contain('/test/fixtures/' + file + '.002');
 
                     internals.removeLog('./test/fixtures/' + file + '.001');
@@ -246,7 +249,7 @@ describe('good-file', function () {
 
                     done();
 
-                }, 1000);
+                }, 2000);
             });
         });
 
@@ -285,7 +288,7 @@ describe('good-file', function () {
 
                         done();
                     });
-                }, 1000);
+                }, 2000);
             });
         });
 
@@ -312,9 +315,11 @@ describe('good-file', function () {
 
                         expect(reporter._currentStream.path).to.contain('/test/fixtures/' + file1 + '.011');
                         expect(reporter._currentStream.bytesWritten).to.equal(29);
+                        expect(reporter._currentStream._good.length).to.equal(29);
 
                         expect(reporterTwo._currentStream.path).to.contain('/test/fixtures/' + file2 + '.001');
                         expect(reporterTwo._currentStream.bytesWritten).to.equal(58);
+                        expect(reporterTwo._currentStream._good.length).to.equal(58);
 
                         internals.removeLog(reporterTwo._currentStream.path);
                         internals.removeLog(reporter._currentStream.path);
@@ -322,8 +327,53 @@ describe('good-file', function () {
 
                         done();
 
-                    }, 1000);
+                    }, 2000);
                 });
+            });
+        });
+
+        it('can handle a large number of events without building back-pressure on the WriteStream', function (done) {
+
+            var file = internals.uniqueFile();
+            var reporter = new GoodFile('./test/fixtures/' + file, {
+                events: {
+                    request: '*'
+                }
+            });
+            var ee = new EventEmitter();
+            var drain = false;
+            var writeCount = 0;
+            var write = Writable.prototype.write;
+
+            Writable.prototype.write = function (chunk, encoding) {
+
+                writeCount++;
+                if (writeCount == 10000) {
+                    reporter._currentStream.end();
+                    internals.removeLog(reporter._currentStream.path);
+                    expect(reporter._currentStream._good.length).to.equal(727707);
+                    Writable.prototype.write = write;
+
+                    return done();
+                }
+                return write.call(this, chunk, encoding);
+            };
+
+
+            reporter.start(ee, function (error) {
+
+                expect(error).to.not.exist;
+                expect(reporter._currentStream.path).to.contain('/test/fixtures/' + file + '.001');
+
+                reporter._currentStream.on('drain', function () {
+
+                    drain = true;
+                    expect(reporter._queue.paused).to.be.true;
+                });
+
+                for (var i = 0; i <= 10000; i++) {
+                    ee.emit('report', 'request', { id: i, timestamp: Date.now(), value: 'value for iteration ' + i });
+                }
             });
         });
     });
